3ee8f7e7dd509025c4d4ca716ec94ebd49bea07e
Zukhriddin <zuxiriddin1995@gmail.com>
2022-07-05 00:00:45 +0500

    lost

--- components/SinglePost.jsx
+++ components/SinglePost.jsx
@@ -12,33 +12,34 @@ const SinglePost = ({ categories, slug }) => {
   }, [])
   console.log(singlePost)
   return (
-    <div>
+    <>
       {singlePost.map((post) => (
         <div className='relative h-72' key={post}>
           <div
             className='absolute rounded-lg bg-center bg-no-repeat bg-cover shadow-lg inline-block w-full h-72 '
             style={{ backgroundImage: `url(${post.featuredImage.url})` }}
           >
-            <div className='absolute rounded-lg bg-center bg-gradient-to-b opacity-50 from-gray-400 via-gray-700 to-black w-full h-72 '></div>
-            <div className='flex flex-col rounded-lg p-4 items-center justify-center absolute w-full h-full'>
-              <p className='text-white mb-4 text-shadow font-semibold text-xs'>
-                {moment(post.createdAt).format('MMM DD YYYY')}
-              </p>
-              <p className='text-white mb-4 text-shadow font-semibold text-2xl text-center'>
-                {post.title}
-              </p>
-              <div className='flex items-center absolute bottom-5 w-full justify-center'>
-                <Image
-                  unoptimized
-                  alt={post.author.name}
-                  height={30}
-                  width={30}
-                  className='align-middle drop-shadow-lg rounded-full'
-                  src={post.author.photo.url}
-                />
-                <p className='inline align-middle text-white text-shadow ml-2 font-medium'>
-                  {post.author.name}
+            <div className='absolute rounded-lg bg-center bg-gradient-to-b opacity-50 from-gray-400 via-gray-700 to-black w-full h-72 '>
+              <div className='flex flex-col rounded-lg p-4 items-center justify-center absolute w-full h-full'>
+                <p className='text-white mb-4 text-shadow font-semibold text-xs'>
+                  {moment(post.createdAt).format('MMM DD YYYY')}
                 </p>
+                <p className='text-white mb-4 text-shadow font-semibold text-2xl text-center'>
+                  {post.title}
+                </p>
+                <div className='flex items-center absolute bottom-5 w-full justify-center'>
+                  <Image
+                    unoptimized
+                    alt={post.author.name}
+                    height={30}
+                    width={30}
+                    className='align-middle drop-shadow-lg rounded-full'
+                    src={post.author.photo.url}
+                  />
+                  <p className='inline align-middle text-white text-shadow ml-2 font-medium'>
+                    {post.author.name}
+                  </p>
+                </div>
               </div>
             </div>
           </div>
@@ -50,6 +51,8 @@ const SinglePost = ({ categories, slug }) => {
                 fill='none'
                 viewBox='0 0 24 24'
                 stroke='currentColor'
+                width={22}
+                height={20}
               >
                 <path
                   strokeLinecap='round'
@@ -62,7 +65,7 @@ const SinglePost = ({ categories, slug }) => {
           </Link>
         </div>
       ))}
-    </div>
+    </>
   )
 }
 
